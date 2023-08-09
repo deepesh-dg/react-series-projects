f80e89bb03d51d722f20b8e10a6cea8c5037d520
Deepesh Gupta <deepesh@findcoder.io>
2023-08-09 17:18:16 +0530

    blogSlice removed

--- blog-app/src/store/store.js
+++ blog-app/src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-import blogSlice from "./blogSlice";
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
-        blog: blogSlice,
     },
 });
 
