0bc35121fdea06aeafab0dc5bb07643fa448cf92
Deepesh Gupta <deepesh@findcoder.io>
2023-08-29 11:39:56 +0530

    bug

--- intro-to-routing/src/components/footer/Footer.jsx
+++ intro-to-routing/src/components/footer/Footer.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer class="bg-white border-y">
-            <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                <div class="md:flex md:justify-between">
-                    <div class="mb-6 md:mb-0">
-                        <Link to="/" class="flex items-center">
+        <footer className="bg-white border-y">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link to="/" className="flex items-center">
                             <img
                                 src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
                                 className="mr-3 h-16"
@@ -15,29 +15,29 @@ export default function Footer() {
                             />
                         </Link>
                     </div>
-                    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
-                            <ul class="text-gray-500 font-medium">
-                                <li class="mb-4">
-                                    <Link to="/" class="hover:underline">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="/" className="hover:underline">
                                         Home
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/about" class="hover:underline">
+                                    <Link to="/about" className="hover:underline">
                                         About
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
-                            <ul class="text-gray-500 font-medium">
-                                <li class="mb-4">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
                                     <a
                                         href="https://github.com/hiteshchoudhary"
-                                        class="hover:underline"
+                                        className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
                                     >
@@ -45,22 +45,22 @@ export default function Footer() {
                                     </a>
                                 </li>
                                 <li>
-                                    <Link to="/" class="hover:underline">
+                                    <Link to="/" className="hover:underline">
                                         Discord
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
-                            <ul class="text-gray-500 font-medium">
-                                <li class="mb-4">
-                                    <Link to="#" class="hover:underline">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="#" className="hover:underline">
                                         Privacy Policy
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="#" class="hover:underline">
+                                    <Link to="#" className="hover:underline">
                                         Terms &amp; Conditions
                                     </Link>
                                 </li>
@@ -68,19 +68,19 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <span class="text-sm text-gray-500 sm:text-center">
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <span className="text-sm text-gray-500 sm:text-center">
                         © 2023{" "}
-                        <a href="https://hiteshchoudhary.com/" class="hover:underline">
+                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
                             hiteshchoudhary
                         </a>
                         . All Rights Reserved.
                     </span>
-                    <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-                        <Link to="#" class="text-gray-500 hover:text-gray-900">
+                    <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+                        <Link to="#" className="text-gray-500 hover:text-gray-900">
                             <svg
-                                class="w-4 h-4"
+                                className="w-4 h-4"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -92,11 +92,11 @@ export default function Footer() {
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                            <span class="sr-only">Facebook page</span>
+                            <span className="sr-only">Facebook page</span>
                         </Link>
-                        <Link to="#" class="text-gray-500">
+                        <Link to="#" className="text-gray-500">
                             <svg
-                                class="w-4 h-4"
+                                className="w-4 h-4"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -104,11 +104,11 @@ export default function Footer() {
                             >
                                 <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
                             </svg>
-                            <span class="sr-only">Discord community</span>
+                            <span className="sr-only">Discord community</span>
                         </Link>
-                        <Link to="#" class="text-gray-500">
+                        <Link to="#" className="text-gray-500">
                             <svg
-                                class="w-4 h-4"
+                                className="w-4 h-4"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -120,11 +120,11 @@ export default function Footer() {
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                            <span class="sr-only">Twitter page</span>
+                            <span className="sr-only">Twitter page</span>
                         </Link>
-                        <Link to="#" class="text-gray-500">
+                        <Link to="#" className="text-gray-500">
                             <svg
-                                class="w-4 h-4"
+                                className="w-4 h-4"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -136,11 +136,11 @@ export default function Footer() {
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                            <span class="sr-only">GitHub account</span>
+                            <span className="sr-only">GitHub account</span>
                         </Link>
-                        <Link to="#" class="text-gray-500">
+                        <Link to="#" className="text-gray-500">
                             <svg
-                                class="w-4 h-4"
+                                className="w-4 h-4"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
@@ -152,7 +152,7 @@ export default function Footer() {
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                            <span class="sr-only">Dribbble account</span>
+                            <span className="sr-only">Dribbble account</span>
                         </Link>
                     </div>
                 </div>
--- intro-to-routing/src/components/home/home.md
+++ intro-to-routing/src/components/home/home.md
@@ -1,15 +1,15 @@
 ```html
 <div className="mx-auto w-full max-w-7xl">
-            <aside class="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
-                <div class="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-                    <div class="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                        <h2 class="text-4xl font-bold sm:text-5xl">
+            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                    <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+                        <h2 className="text-4xl font-bold sm:text-5xl">
                             Download Now
-                            <span class="hidden sm:block text-4xl">Lorem Ipsum</span>
+                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
                         </h2>
 
                         <Link
-                            class="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+                            className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
                             to="/"
                         >
                             <svg
@@ -27,15 +27,15 @@
                     </div>
                 </div>
 
-                <div class="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img class="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
                 </div>
             </aside>
 
-            <div class="grid  place-items-center sm:mt-20">
-                <img class="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+            <div className="grid  place-items-center sm:mt-20">
+                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
             </div>
 
-            <h1 class="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
 ```
